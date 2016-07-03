459948b2649a95b182d6ce38617b584733afb21d
JimmyLegend <1479426478@qq.com>
2016-07-03 22:28:52 +0800

    2016/7/3/22:28

--- PhoneKeeper/src/com/example/phonekeeper/BlackListActivity.java
+++ PhoneKeeper/src/com/example/phonekeeper/BlackListActivity.java
@@ -6,13 +6,20 @@ import edu.sdut.phonekeeper.db.dao.BlackNumberDao;
 import edu.sdut.phonekeeper.domain.BlackNumInfo;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -27,6 +34,57 @@ public class BlackListActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_black_list);
+		lvBlackList=(ListView) findViewById(R.id.lv_blacklist);
+		btnAddBlack=(Button) findViewById(R.id.btn_addblack);
+		blackDao=new BlackNumberDao(BlackListActivity.this);
+		arrBlack=blackDao.findAll();
+		blackAdapter=new BlackAdapter();
+		lvBlackList.setAdapter(blackAdapter);
+		lvBlackList.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+				final BlackNumInfo blackNum=arrBlack.get(arg2);
+				new AlertDialog.Builder(BlackListActivity.this).setMessage("确定要删除["+blackNum.getNum()+"]吗？").setTitle("提示").setPositiveButton("确定",new DialogInterface.OnClickListener() {
+					
+					@Override
+					public void onClick(DialogInterface arg0, int arg1) {
+						// TODO Auto-generated method stub
+						blackDao.delete(blackNum.getNum());
+						arrBlack=blackDao.findAll();
+						blackAdapter.notifyDataSetChanged();
+						arg0.dismiss();
+						
+					}
+				}).setNegativeButton("取消",null).create().show();
+				
+			}
+			
+		});
+		
+		btnAddBlack.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				View view=View.inflate(BlackListActivity.this,R.layout.view_blacknum,null);
+				final EditText etBlackNum=(EditText) view.findViewById(R.id.et_blacknum);
+				new AlertDialog.Builder(BlackListActivity.this).setTitle("添加").setView(view).setPositiveButton("确定",new DialogInterface.OnClickListener() {
+					
+					@Override
+					public void onClick(DialogInterface arg0, int arg1) {
+						// TODO Auto-generated method stub
+						String blackNum=etBlackNum.getText().toString();
+						blackDao.add(blackNum);
+						arrBlack=blackDao.findAll();
+						blackAdapter.notifyDataSetChanged();
+						arg0.dismiss();
+					}
+				}).setNegativeButton("取消",null).create().show();
+			}
+		});
 	}
 
 	@Override
